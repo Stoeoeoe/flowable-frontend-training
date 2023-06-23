9feec228647e74c02c421c57817decad9d7cbfb7
matthias.stoeckli <matthias.stoeckli@flowable.com>
2023-06-23 10:29:09 +0200

    Update main

--- frontend/src/form-components/suite.tsx
+++ frontend/src/form-components/suite.tsx
@@ -1,7 +1,5 @@
 import {HelloWorld} from "./HelloWorld/HelloWorld";
-import {FlowableMap} from "./FlowableMap/FlowableMap";
 
 export default {
-    trainingHelloworld: HelloWorld,
-    trainingFlowableMap: FlowableMap
+    trainingHelloworld: HelloWorld
 }
